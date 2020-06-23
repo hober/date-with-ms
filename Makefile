ca6919a88be121be612928371bb971a5e55fa6a6
Theresa O'Connor <hober@apple.com>
2020-06-23 13:33:43 -0700

    Initial version.

---
+++ Makefile
@@ -0,0 +1,16 @@
+BINARY=date-with-ms
+
+$(BINARY): date-with-ms.c
+	clang $< -o $@
+
+# $SYSNAME is set in my shell config; it's where I put binaries I've
+# built myself.
+install: $(BINARY)
+	mkdir -p ~/${SYSNAME}/bin
+	cp $< ~/${SYSNAME}/bin
+
+clean:
+	rm -f $(BINARY) *~
+
+.PHONY: clean install
+
---
+++ date-with-ms.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+int main() {
+  struct timeval tv_now;
+  gettimeofday(&tv_now, NULL);
+
+  char without_ms[32];
+  strftime(without_ms, sizeof without_ms, "%Y-%m-%d %H:%M:%S",
+           localtime(&tv_now.tv_sec));
+
+  long milliseconds = tv_now.tv_usec / 1000;
+
+  printf("%s,%03ld\n", without_ms, milliseconds);
+
+  return 0;
+}
